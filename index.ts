fc43391e6ec57d7b76555339ad3092ab096442b7
sean-b765 <sean.b0153@gmail.com>
2022-02-27 11:24:51 +0800

    Update

--- index.ts
+++ index.ts
@@ -1,105 +1,40 @@
-import axios from 'axios'
-import { parseString } from 'xml2js'
 import express from 'express'
-
-async function getFuelData() {
-	return new Promise<any>((resolve, reject) => {
-		axios
-			.get('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS')
-			.then((res) => {
-				const json = parseString(res.data, (err, result) => {
-					resolve(result.rss.channel[0].item)
-				})
-			})
-			.catch((err) => {
-				reject(err)
-			})
-	})
-}
-
-function degreesToRad(deg) {
-	return deg * (Math.PI / 180)
-}
-
-async function getJourney(origin, destination) {
-	return new Promise((resolve, reject) => {
-		axios
-			.get(
-				`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&mode=driving&language=en-US&key=${process.env.KEY}`
-			)
-			.then((res) => {
-				resolve(res.data.rows[0].elements)
-			})
-			.catch((err) => reject(err))
-	})
-}
-
-function distanceBetweenCoords(latlng1: number[], latlng2: number[]) {
-	/*
-    https://en.wikipedia.org/wiki/Haversine_formula
-  */
-	const R = 6371
-	const deltaLat = degreesToRad(latlng1[0] - latlng2[0])
-	const deltaLng = degreesToRad(latlng1[1] - latlng2[1])
-
-	const a =
-		Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
-		Math.cos(degreesToRad(latlng1[0])) *
-			Math.cos(degreesToRad(latlng2[0])) *
-			Math.sin(deltaLng / 2) *
-			Math.sin(deltaLng / 2)
-	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-	const d = R * c
-
-	return d
-}
+import dotenv from 'dotenv'
+import { parseCoords } from './helpers'
+import { getFuelData, getJourney } from './apis'
+import rateLimit from 'express-rate-limit'
+dotenv.config()
 
 const app = express()
 
+app.use(
+	rateLimit({
+		windowMs: 5 * 60 * 1000,
+		max: 50,
+		standardHeaders: true,
+		legacyHeaders: false,
+	})
+)
 app.use(express.json())
 
-app.get('/:coords', async (req, res) => {
+app.get('/nearest/:coords', async (req, res) => {
 	const { coords } = req.params
 
-	let lat: number, lng: number
-	try {
-		lat = Number(coords.split(',')[0])
-		lng = Number(coords.split(',')[1])
+	const { lat, lng, error } = parseCoords(coords)
 
-		if (isNaN(lat) || isNaN(lng)) throw TypeError()
-	} catch (err) {
-		return res
-			.status(400)
-			.json({ message: 'The coordinates you supplied are invalid.' })
-	}
+	if (error) return res.status(400).json({ message: 'Invalid coordinates.' })
 
-	console.log(lat, lng)
+	const result: { result: any[]; error?: boolean } = await getFuelData(lat, lng)
 
-	const result: any[] = await getFuelData()
-	for (let i = 0; i < result.length; i++) {
-		// xml2js package returns all values as [ 'string' ], convert into just 'string'
-		for (let j = 0; j < Object.values(result[i]).length; j++) {
-			const value = Object.values(result[i])[j]
-			const key = Object.keys(result[i])[j]
+	if (result.error)
+		return res.status(500).json({ message: 'Internal Server Error' })
 
-			// Attempt to convert the value into number
-			const _value = Number(value[0])
+	const sorted = result.result.sort(
+		(objA, objB) => objA.distanceTo - objB.distanceTo
+	)
 
-			result[i][key] = isNaN(_value) ? value[0] : _value
-		}
-
-		// distance user's coordinates and servo coordinates
-		const distanceBetween = distanceBetweenCoords(
-			[result[i].latitude, result[i].longitude],
-			[lat, lng]
-		)
-		result[i].distanceTo = distanceBetween
-	}
-
-	result.sort((objA, objB) => objA.distanceTo - objB.distanceTo)
-
-	const nearest = result[0]
+	const nearest = sorted[0]
+	if (!nearest) return res.status(400).json({ message: 'No results nearby' })
 
 	// real journey time from google maps API
 	const journey = await getJourney(
@@ -107,10 +42,47 @@ app.get('/:coords', async (req, res) => {
 		`${lat},${lng}`
 	)
 
-	nearest.distance = journey[0].distance.text
-	nearest.duration = journey[0].duration.text
+	if (journey[0]?.status !== 'ZERO_RESULTS') {
+		nearest.distance = journey[0].distance.text
+		nearest.duration = journey[0].duration.text
+	}
 
-	return res.json({ lat, lng, nearest })
+	return res.json({ nearest })
+})
+
+app.get('/nearestAndCheapest/:coords', async (req, res) => {
+	const { coords } = req.params
+	let radius = Number(req.query.radius || '50')
+
+	const { lat, lng, error } = parseCoords(coords)
+
+	if (error) return res.status(400).json({ message: 'Invalid coordinates.' })
+
+	const result: { result: any[]; error?: boolean } = await getFuelData(
+		lat,
+		lng,
+		radius
+	)
+
+	if (result.error)
+		return res.status(500).json({ message: 'Internal Server Error' })
+
+	if (result.result.length === 0)
+		return res.status(400).json({ message: 'No results in your search radius' })
+
+	const sorted = result.result.sort((objA, objB) => objA.price - objB.price)
+
+	// get google maps journey
+	for (let i = 0; i < 5; i++) {
+		const journey = await getJourney(
+			`${lat},${lng}`,
+			`${sorted[i].latitude},${sorted[i].longitude}`
+		)
+		sorted[i].distance = journey[0]?.distance?.text
+		sorted[i].duration = journey[0]?.duration?.text
+	}
+
+	res.status(200).json({ result: sorted })
 })
 
 const PORT = process.env.PORT || 5000
